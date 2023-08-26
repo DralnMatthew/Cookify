81c4b81dea472f3f3c5fa392e410f02f1b7e2864
matt <zhanghu.zhao@outlook.com>
2023-08-27 01:01:45 +0300

    deployment

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,2 +1,3 @@
-const BASE_URL = "http://localhost:9080/api/v1";
+// const BASE_URL = "http://localhost:9080/api/v1";
+const BASE_URL = "https://cookify-api-v1.onrender.com/api/v1";
 export default BASE_URL;
